9f021f39045a12270d1283260ab6656db459c492
shoishizaki <shoishizaki1997@gmail.com>
2020-10-25 01:03:09 +0900

    start development

---
+++ app/javascript/app.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <top/>
+  </div>
+</template>
+
+<script>
+import top from "./components/top";
+
+export default {
+  components: {
+    top: top
+  }
+}
+</script>
+
+<style scoped>
+</style>
